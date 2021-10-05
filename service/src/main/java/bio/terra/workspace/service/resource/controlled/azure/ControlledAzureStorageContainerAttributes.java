33bdc5869d33c9907d463594d55bcb1ca5f03380
Yev Bronshteyn <yev.bronshteyn@microsoft.com>
2021-10-05 19:41:04 -0400

    Adding azure storage SDK, storage step end-to-end (prototype) - need to incorporate parameters and clean up their schema

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/azure/ControlledAzureStorageContainerAttributes.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/azure/ControlledAzureStorageContainerAttributes.java
@@ -4,15 +4,29 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ControlledAzureStorageContainerAttributes {
-  private final String containerName;
+    private final String containerName;
+    private final String storageAccountName;
+    private final String azureEnvironment;
 
-  @JsonCreator
-  public ControlledAzureStorageContainerAttributes(
-      @JsonProperty("container") String containerName) {
-    this.containerName = containerName;
-  }
+    @JsonCreator
+    public ControlledAzureStorageContainerAttributes(
+            @JsonProperty("storageAccount") String storageAccountName,
+            @JsonProperty("container") String containerName,
+            @JsonProperty("azureEnvironment") String azureEnvironment) {
+        this.storageAccountName = storageAccountName;
+        this.containerName = containerName;
+        this.azureEnvironment = azureEnvironment;
+    }
 
-  public String getContainerName() {
-    return containerName;
-  }
+    public String getContainerName() {
+        return containerName;
+    }
+
+    public String getStorageAccountName() {
+        return storageAccountName;
+    }
+
+    public String getAzureEnvironment() {
+        return azureEnvironment;
+    }
 }
