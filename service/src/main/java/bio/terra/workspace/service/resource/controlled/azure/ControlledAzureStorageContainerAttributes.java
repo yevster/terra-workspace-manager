a34fd90ba3511873ba9ebc48233d8d01996e78bb
Yev Bronshteyn <yev.bronshteyn@microsoft.com>
2021-10-06 17:27:22 -0400

    API object fixes, etc

--- service/src/main/java/bio/terra/workspace/service/resource/controlled/azure/ControlledAzureStorageContainerAttributes.java
+++ service/src/main/java/bio/terra/workspace/service/resource/controlled/azure/ControlledAzureStorageContainerAttributes.java
@@ -4,23 +4,22 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ControlledAzureStorageContainerAttributes {
-    private final String containerName;
-    private final String storageAccountName;
+  private final String containerName;
+  private final String storageAccountName;
 
-    @JsonCreator
-    public ControlledAzureStorageContainerAttributes(
-            @JsonProperty("storageAccount") String storageAccountName,
-            @JsonProperty("container") String containerName) {
-        this.storageAccountName = storageAccountName;
-        this.containerName = containerName;
-    }
+  @JsonCreator
+  public ControlledAzureStorageContainerAttributes(
+      @JsonProperty("storageAccount") String storageAccountName,
+      @JsonProperty("container") String containerName) {
+    this.storageAccountName = storageAccountName;
+    this.containerName = containerName;
+  }
 
-    public String getContainerName() {
-        return containerName;
-    }
+  public String getContainerName() {
+    return containerName;
+  }
 
-    public String getStorageAccountName() {
-        return storageAccountName;
-    }
-
+  public String getStorageAccountName() {
+    return storageAccountName;
+  }
 }
